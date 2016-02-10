015cff894d333905d211923e9b2e60dcfc141d78
Kisun Pokharel <pokharel.kisun@gmail.com>
2016-02-10 13:40:31 +0200

    Update slurm_wrapper.sh

--- slurm_scripts/slurm_wrapper.sh
+++ slurm_scripts/slurm_wrapper.sh
@@ -8,17 +8,17 @@
 ############################################################################
 #!/bin/bash 
 
-Fastqc1=$(sbatch fastqc_pretrim.sh | cut -f 4 -d' ')
+Fastqc1=$(sbatch fastqc_pretrim.sh --array=1-30| cut -f 4 -d' ') #change the --array value based on number of samples
 echo $Fastqc1
-Trimmomatic=$(sbatch -d afterok:$Fastqc1 trimmomatic.sh | cut -f 4 -d' ')
+Trimmomatic=$(sbatch -d afterok:$Fastqc1 trimmomatic.sh --array=1-30| cut -f 4 -d' ')
 echo $Trimmomatic
-Fastqc2=$(sbatch -d afterok:$Trimmomatic fastqc_posttrim.sh | cut -f 4 -d' ')
+Fastqc2=$(sbatch -d afterok:$Trimmomatic fastqc_posttrim.sh --array=1-30| cut -f 4 -d' ')
 echo $Fastqc2
-Tophat2=$(sbatch -d afterok:$Fastqc2 tophat2.sh | cut -f 4 -d' ')
+Tophat2=$(sbatch -d afterok:$Fastqc2 tophat2.sh --array=1-30| cut -f 4 -d' ')
 echo $Tophat2
-Cufflinks=$(sbatch -d afterok:$Tophat2 cufflinks.sh | cut -f 4 -d' ')
+Cufflinks=$(sbatch -d afterok:$Tophat2 cufflinks.sh --array=1-30| cut -f 4 -d' ')
 echo $Cufflinks
-Htseqcount=$(sbatch -d afterok:$Tophat2 htseqcount.sh | cut -f 4 -d' ')
+Htseqcount=$(sbatch -d afterok:$Tophat2 htseqcount.sh --array=1-30| cut -f 4 -d' ')
 echo $Htseqcount
 exit 0
 
