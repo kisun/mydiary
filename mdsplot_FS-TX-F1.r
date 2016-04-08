9527984372448fb9862aed79888b52a4902e5d1b
Kisun Pokharel <pokharel.kisun@gmail.com>
2016-04-08 16:59:15 +0300

    new

--- mdsplot_FS-TX-F1.r
+++ mdsplot_FS-TX-F1.r
@@ -12,7 +12,8 @@ manhattan(gwasResults, genomewideline=F, suggestiveline=F, ylim=c(0,10), cex=0.2
 qq(gwasResults$P)
 
 
-mds<-read.table("IBS-MDS/MDS_FS-TX-F1.mds", header=T)
+mds<-read.table("IBS-MDS/MDS_FS-TX-F1_mibs.mds", header=T)
+head(mds)
 ids<-read.table("IBS-MDS/familyid.csv", header=T)
 colnames(ids)<-c("FID", "FS", "TX", "F1", "Breed")
 Breed<-ids$Breed
@@ -20,6 +21,17 @@ mds1<-data.frame(mds, Breed)
 qplot(C1, C2, data=mds1, color=Breed, alpha=I(0.5), size=I(3))
 
 
+mds_king<-read.table("/media/ejo129/somics/Data/SNP_genotype_data/Release_020414/Oct15/FS-TX-F1/kingpc1.csv", header=T)
+head(mds_king)
+mds_king1<-data.frame(mds_king, Breed)
+qplot(C1, C2, data=mds_king1, color = Breed, alpha=I(0.5), size=I(3))
 
+FS<-1:11
+TX<-12:22
+F1<-23:31
+
+
+
+dev.off()
 
 
